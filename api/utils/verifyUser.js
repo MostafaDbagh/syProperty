e0f479cc0fef7ee63b56269afe04f14188d0585f
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2025-05-28 00:38:14 +0400

    first commit

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req, res, next) => {
+
+  console.log(req,'reeeeeeqqqqqq')
+  const token = req.cookies.access_token;
+
+  if (!token) return next(errorHandler(401, 'Unauthorized'));
+
+  jwt.verify(token, "5345jkj5kl34j5kl34j5", (err, user) => {
+    if (err) return next(errorHandler(403, 'Forbidden'));
+
+    req.user = user;
+    next();
+  });
+};
