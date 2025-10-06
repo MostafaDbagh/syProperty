617bd041c66ac2c6ca273045dcb1b20066b15ce7
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2025-10-06 16:07:49 +0400

    replace svg to compoennts

---
+++ proty-nextjs/components/icons/index.js
@@ -0,0 +1,25 @@
+// Auth & User Icons
+export { default as UserIcon } from './UserIcon';
+export { default as UserAvatarIcon } from './UserAvatarIcon';
+export { default as EmailIcon } from './EmailIcon';
+export { default as LockIcon } from './LockIcon';
+export { default as PhoneIcon } from './PhoneIcon';
+
+// Dashboard Icons
+export { default as DashboardIcon } from './DashboardIcon';
+export { default as ProfileIcon } from './ProfileIcon';
+export { default as PackageIcon } from './PackageIcon';
+export { default as HeartIcon } from './HeartIcon';
+export { default as ReviewIcon } from './ReviewIcon';
+export { default as PropertyIcon } from './PropertyIcon';
+export { default as AddPropertyIcon } from './AddPropertyIcon';
+export { default as LogoutIcon } from './LogoutIcon';
+
+// Property Action Icons
+export { default as HeartOutlineIcon } from './HeartOutlineIcon';
+export { default as CompareIcon } from './CompareIcon';
+export { default as PrintIcon } from './PrintIcon';
+export { default as ShareIcon } from './ShareIcon';
+
+// Usage: import { UserIcon, EmailIcon, DashboardIcon, HeartOutlineIcon } from '@/components/icons';
+
