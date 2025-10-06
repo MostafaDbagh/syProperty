ab36b4d71100836a7af4c434ba00be5b5ec1d456
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2025-10-06 13:24:59 +0400

    fix

--- proty-nextjs/components/propertyDetails/RelatedProperties.jsx
+++ proty-nextjs/components/propertyDetails/RelatedProperties.jsx
@@ -2,10 +2,9 @@
 import React, { useMemo } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import { useSearchListings } from "@/apis/hooks";
 import { getPropertyImage, getPropertyTitle } from "@/utlis/propertyHelpers";
+import styles from "./RelatedProperties.module.css";
 
 export default function RelatedProperties({ currentProperty }) {
   // Calculate search parameters for similar properties
@@ -79,6 +78,7 @@ export default function RelatedProperties({ currentProperty }) {
   if (isError || similarProperties.length === 0) {
     return null; // Don't show the section if no similar properties
   }
+
   return (
     <section className="section-similar-properties tf-spacing-3">
       <div className="tf-container">
@@ -87,185 +87,76 @@ export default function RelatedProperties({ currentProperty }) {
             <div className="heading-section mb-32">
               <h2 className="title">Similar Properties</h2>
             </div>
-            <div
-              className="swiper style-pagination tf-sw-mobile-1 sw-swiper-992"
-              data-screen={992}
-              data-preview={1}
-              data-space={15}
-            >
-              <div
-                className="swiper-wrapper tf-layout-mobile-xl lg-col-3 wrap-agent wow fadeInUp"
-                data-wow-delay=".2s"
-              >
-                {similarProperties.map((property) => (
-                    <div className="swiper-slide" key={property._id}>
-                      <div className="box-house hover-img">
-                        <div className="image-wrap">
-                          <Link href={`/property-detail/${property._id}`}>
-                            <Image
-                              className="lazyload"
-                              data-src={getPropertyImage(property)}
-                              alt={getPropertyTitle(property)}
-                              src={getPropertyImage(property)}
-                              width={615}
-                              height={405}
-                            />
-                          </Link>
-                          <ul className="box-tag flex gap-8">
-                            {property.offer && (
-                              <li className="flat-tag text-4 bg-main fw-6 text_white">
-                                Special Offer
-                              </li>
-                            )}
-                            {property.status === 'sale' && (
-                              <li className="flat-tag text-4 bg-3 fw-6 text_white">
-                                For Sale
-                              </li>
-                            )}
-                            {property.status === 'rent' && (
-                              <li className="flat-tag text-4 bg-primary fw-6 text_white">
-                                For Rent
-                              </li>
-                            )}
-                          </ul>
-                          <div className="list-btn flex gap-8">
-                            <a href="#" className="btn-icon save hover-tooltip">
-                              <i className="icon-save" />
-                              <span className="tooltip">Add Favorite</span>
-                            </a>
-                            <a href="#" className="btn-icon find hover-tooltip">
-                              <i className="icon-find-plus" />
-                              <span className="tooltip">Quick View</span>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="content">
-                          <h5 className="title">
-                            <Link href={`/property-detail/${property._id}`}>
-                              {getPropertyTitle(property)}
-                            </Link>
-                          </h5>
-                          <p className="location text-1 flex items-center gap-8">
-                            <i className="icon-location" /> {property.address}, {property.state}
-                          </p>
-                          <ul className="meta-list flex">
-                            <li className="text-1 flex">
-                              <span>{property.bedrooms || 0}</span>Beds
-                            </li>
-                            <li className="text-1 flex">
-                              <span>{property.bathrooms || 0}</span>Baths
-                            </li>
-                            <li className="text-1 flex">
-                              <span>{property.size || 0}</span>Sqft
-                            </li>
-                          </ul>
-                          <div className="bot flex justify-between items-center">
-                            <h5 className="price">${property.propertyPrice?.toLocaleString()}</h5>
-                            <div className="wrap-btn flex">
-                              <Link
-                                href={`/property-detail/${property._id}`}
-                                className="tf-btn style-border pd-4"
-                              >
-                                Details
-                              </Link>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                ))}
-              </div>
-              <div className="sw-pagination sw-pagination-mb-1 text-center d-lg-none d-block mt-20" />
-            </div>
-            <Swiper
-              className="swiper style-pagination tf-sw-mobile-1 sw-swiper-992"
-              spaceBetween={15}
-              modules={[Pagination]}
-              pagination={{
-                clickable: true,
-                el: ".spd458",
-              }}
-            >
-              {similarProperties.map((property) => (
-                  <SwiperSlide className="swiper-slide" key={property._id}>
-                    <div className="box-house hover-img">
-                      <div className="image-wrap">
-                        <Link href={`/property-detail/${property._id}`}>
-                          <Image
-                            className="lazyload"
-                            data-src={getPropertyImage(property)}
-                            alt={getPropertyTitle(property)}
-                            src={getPropertyImage(property)}
-                            width={615}
-                            height={405}
-                          />
-                        </Link>
-                        <ul className="box-tag flex gap-8">
-                          {property.offer && (
-                            <li className="flat-tag text-4 bg-main fw-6 text_white">
-                              Special Offer
-                            </li>
-                          )}
-                          {property.status === 'sale' && (
-                            <li className="flat-tag text-4 bg-3 fw-6 text_white">
-                              For Sale
-                            </li>
-                          )}
-                          {property.status === 'rent' && (
-                            <li className="flat-tag text-4 bg-primary fw-6 text_white">
-                              For Rent
-                            </li>
-                          )}
-                        </ul>
-                        <div className="list-btn flex gap-8">
-                          <a href="#" className="btn-icon save hover-tooltip">
-                            <i className="icon-save" />
-                            <span className="tooltip">Add Favorite</span>
-                          </a>
-                          <a href="#" className="btn-icon find hover-tooltip">
-                            <i className="icon-find-plus" />
-                            <span className="tooltip">Quick View</span>
-                          </a>
-                        </div>
-                      </div>
-                      <div className="content">
-                        <h5 className="title">
-                          <Link href={`/property-detail/${property._id}`}>
-                            {getPropertyTitle(property)}
-                          </Link>
-                        </h5>
-                        <p className="location text-1 flex items-center gap-8">
-                          <i className="icon-location" /> {property.address}, {property.state}
-                        </p>
-                        <ul className="meta-list flex">
-                          <li className="text-1 flex">
-                            <span>{property.bedrooms || 0}</span>Beds
-                          </li>
-                          <li className="text-1 flex">
-                            <span>{property.bathrooms || 0}</span>Baths
-                          </li>
-                          <li className="text-1 flex">
-                            <span>{property.size || 0}</span>Sqft
-                          </li>
-                        </ul>
-                        <div className="bot flex justify-between items-center">
-                          <h5 className="price">${property.propertyPrice?.toLocaleString()}</h5>
-                          <div className="wrap-btn flex">
-                            <Link
-                              href={`/property-detail/${property._id}`}
-                              className="tf-btn style-border pd-4"
-                            >
-                              Details
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-              ))}
+            <div className={styles.propertiesGrid}>
+              {similarProperties.map((property) => {
+                const getBadgeClass = () => {
+                  const status = property.status?.toLowerCase();
+                  if (status === 'rent') return styles.forRent;
+                  if (status === 'sale') return styles.forSale;
+                  return '';
+                };
 
-              <div className="sw-pagination sw-pagination-mb-1 text-center d-lg-none d-block mt-20 spd458" />
-            </Swiper>
+                return (
+                  <div key={property._id} className={styles.propertyCard}>
+                    <div className={styles.imageWrapper}>
+                      <Link href={`/property-detail/${property._id}`}>
+                        <Image
+                          src={getPropertyImage(property)}
+                          alt={getPropertyTitle(property)}
+                          width={400}
+                          height={300}
+                          className={styles.propertyImage}
+                        />
+                      </Link>
+                      
+                      {property.offer && (
+                        <span className={styles.offerBadge}>Special Offer</span>
+                      )}
+                      
+                      {property.status && (
+                        <span className={`${styles.statusBadge} ${getBadgeClass()}`}>
+                          {property.status === 'sale' ? 'For Sale' : 'For Rent'}
+                        </span>
+                      )}
+                    </div>
+
+                    <div className={styles.cardContent}>
+                      <h3 className={styles.propertyTitle}>
+                        <Link href={`/property-detail/${property._id}`}>
+                          {getPropertyTitle(property)}
+                        </Link>
+                      </h3>
+
+                      <div className={styles.location}>
+                        <i className="icon-location" />
+                        <span>{property.address}, {property.state}</span>
+                      </div>
+
+                      <div className={styles.metaInfo}>
+                        <div className={styles.metaItem}>
+                          <span>{property.bedrooms || 0}</span> Beds
+                        </div>
+                        <div className={styles.metaItem}>
+                          <span>{property.bathrooms || 0}</span> Baths
+                        </div>
+                        <div className={styles.metaItem}>
+                          <span>{property.size || 0}</span> Sqft
+                        </div>
+                      </div>
+
+                      <div className={styles.cardFooter}>
+                        <h4 className={styles.price}>
+                          ${property.propertyPrice?.toLocaleString()}
+                        </h4>
+                        <Link href={`/property-detail/${property._id}`} className={styles.detailsBtn}>
+                          View Details
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
